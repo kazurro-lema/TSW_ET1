3c864f06f09bc99a38af300adadf415efe046bf7
Samuel Lema González <kazurrolema@gmail.com>
2022-10-27 11:54:32 +0200

    Añadido estilo de pantalla de registro

--- view/users/register.php
+++ view/users/register.php
@@ -1,21 +1,54 @@
 <?php
-//file: view/users/register.php
 
-require_once(__DIR__."/../../core/ViewManager.php");
+require_once(__DIR__ . "/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
 $errors = $view->getVariable("errors");
 $user = $view->getVariable("user");
 $view->setVariable("title", "Register");
 ?>
-<h1><?= i18n("Register")?></h1>
-<form action="index.php?controller=users&amp;action=register" method="POST">
-	<?= i18n("Username")?>: <input type="text" name="username"
-	value="<?= $user->getUsername() ?>">
-	<?= isset($errors["username"])?i18n($errors["username"]):"" ?><br>
 
-	<?= i18n("Password")?>: <input type="password" name="passwd"
-	value="">
-	<?= isset($errors["passwd"])?i18n($errors["passwd"]):"" ?><br>
+<div class="login-page">
+	<div class="login-header">
+		<div class="bc-logo">
+			<img src="/login/auth/resources/vnghb/login/biccloud/img/bic-cloud-logo-login.png">
+		</div>
+		<div class="bc-navigation">
+			<div class="login-lang kc-locale">
+				<?php include(__DIR__ . "/../layouts/toggleLang.php"); ?>
+			</div>
+		</div>
+	</div>
+	<div class="bc-content">
+		<div class="bc-flex-container">
+			<div class="bc-msg">
+				<h1 class="title">
+					<span class="hide-on-mobile">Registrate</span>
+				</h1>
+			</div>
+			<div class="login-form">
+				<h4 class="form-header"></h4>
 
-	<input type="submit" value="<?= i18n("Register")?>">
-</form>
+				<div class="bc-login-form">
+					<form id="kc-login-form" action="index.php?controller=users&amp;action=register" method="POST">
+						<div class="inputs-container">
+							<div class="input-content">
+								<input tabindex="1" id="username" class="input user" name="username" placeholder="<?= i18n("Put_Username") ?>" value="" type="text" autofocus="" autocomplete="off">
+							</div>
+							<div class="input-content">
+								<input tabindex="2" id="password" class="input pw" name="passwd" placeholder="<?= i18n("Put_Password") ?>" type="password" autocomplete="off">
+							</div>
+							<div class="submit-content hide-on-mobile">
+								<input tabindex="4" class="submit" name="login" id="kc-login" type="submit" value="<?= i18n("Register") ?>">
+							</div>
+						</div>
+						<div class="inputs-container show-on-mobile">
+							<div class="submit-content">
+								<input tabindex="4" class="submit" name="login" id="kc-login" type="submit" value="<?= i18n("Register") ?>">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
