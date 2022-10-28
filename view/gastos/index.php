0fabfdee92e4e5596ac8f2f6d4c272c219192025
lauuu2210 <lplopez19@esei.uvigo.es>
2022-10-28 22:43:23 +0200

    Si es que soy la mejor programadora del mundo

--- view/gastos/index.php
+++ view/gastos/index.php
@@ -21,31 +21,32 @@ $view->setVariable("title", "Gastos");
     </tr>
 
     <?php foreach ($gastos as $gasto) : ?>
-        <tr>
-            <td>
-                <a href="index.php?controller=gastos&amp;action=view&amp;id=<?= $gasto->getId() ?>"><?= htmlentities($gasto->getNombreGasto()) ?></a>
-            </td>
-            <td>
-                <?= $gasto->getCantidadGasto() ?>
-            </td>
-            <td>
-                <?= $gasto->getTipo() ?>
-            </td>
-            <td>
-                <?= $gasto->getEntidad() ?>
-            </td>
-            <td>
-                <?= $gasto->getFecha() ?>
-            </td>
-            <td>
-                <?= $gasto->getDescripcion() ?>
-            </td>
-            <td>
-                <?= $gasto->getFichero() ?>
-            </td>
-            <td>
-                <?php
-                if (isset($currentuser) && $currentuser == $gasto->getAuthor()->getUsername()) : ?>
+        <?php
+        if (isset($currentuser) && $currentuser == $gasto->getAuthor()->getUsername()) : ?>
+            <tr>
+                <td>
+                    <a href="index.php?controller=gastos&amp;action=view&amp;id=<?= $gasto->getId() ?>"><?= htmlentities($gasto->getNombreGasto()) ?></a>
+                </td>
+                <td>
+                    <?= $gasto->getCantidadGasto() ?>
+                </td>
+                <td>
+                    <?= $gasto->getTipo() ?>
+                </td>
+                <td>
+                    <?= $gasto->getEntidad() ?>
+                </td>
+                <td>
+                    <?= $gasto->getFecha() ?>
+                </td>
+                <td>
+                    <?= $gasto->getDescripcion() ?>
+                </td>
+                <td>
+                    <?= $gasto->getFichero() ?>
+                </td>
+                <td>
+
                     <?php
                     ?>
                     <form method="POST" action="index.php?controller=gastos&amp;action=delete" id="delete_gasto_<?= $gasto->getId(); ?>" style="display: inline">
@@ -64,8 +65,9 @@ $view->setVariable("title", "Gastos");
                     ?>
                     <a href="index.php?controller=gastos&amp;action=edit&amp;id=<?= $gasto->getId() ?>"><?= i18n("Edit") ?></a>
 
-                <?php endif; ?>
-            </td>
-        </tr>
+
+                </td>
+            </tr>
+        <?php endif; ?>
     <?php endforeach; ?>
 </table>
